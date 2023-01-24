1fab0094f818c6a74cb2efb5cba984f3a4e13e39
klocinho <lubiejedzeniexd@gmail.com>
2023-01-24 20:28:12 +0100

    Klocek zrobił menu!!!! (w końcu)

---
+++ Assets/SKRYPTY/Menu/Manager/ScenesManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public enum Scene
+    {
+        MainMenu,
+        Level01
+    }
+
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+    //ScenesManager.Instance.LoadScene(ScenesManager.Scene.MainMenu); do zaladowania jakiejkolwiek sceny, w tym przypadku main menu
+
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(Scene.Level01.ToString());
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(Scene.MainMenu.ToString());
+    }
+}
---
+++ Assets/SKRYPTY/Menu/UI/UIGameMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIGameMenu : MonoBehaviour
+{
+    [SerializeField] Button _mainMenu;
+
+    void Start()
+    {
+        _mainMenu.onClick.AddListener(LoadMainMenu);
+    }
+
+    private void LoadMainMenu()
+    {
+        ScenesManager.Instance.LoadMainMenu();
+    }
+}
