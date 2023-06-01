b2e63a4a9cbf20c64034a5f78dc36511c5f71a3f
HabcioN <118073469+HabcioN@users.noreply.github.com>
2023-06-01 18:39:27 +0200

    przeskalowanie smierci (teraz jest idealnie)

--- Assets/Skrypty/DO NURKA/Bullet.cs
+++ Assets/Skrypty/DO NURKA/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
-    public int damage = 40;
+    public int damage = 50;
     void Start()
     {
         rb.velocity = transform.right * speed;
