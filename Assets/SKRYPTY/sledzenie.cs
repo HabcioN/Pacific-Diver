0863285327fc113449dd52ae1ef28d5fd1c4b322
wilczek10 <wilczynski.gabrys@gmail.com>
2023-06-05 21:49:27 +0200

    GABRYŚ JEST WIELKI KAMERA

--- Assets/SKRYPTY/sledzenie.cs
+++ Assets/SKRYPTY/sledzenie.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class sledzenie : MonoBehaviour
 {
 
-    public float FollowSpeed = 2f;
+    public float followSpeed = 2f;
     public float yOffset = 1f;
-    public Transform postac;
+    public Transform target;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
-    void Update()
+    private void LateUpdate()
     {
-        Vector3 newPos = new Vector3(postac.position.x, transform.position.y, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        if (target != null)
+        {
+            float targetX = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
+            float targetY = Mathf.Clamp(target.position.y + yOffset, minPosition.y, maxPosition.y);
+            Vector3 newPos = new Vector3(targetX, targetY, transform.position.z);
+            transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
+        }
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(minPosition.x, minPosition.y, transform.position.z), new Vector3(minPosition.x, maxPosition.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(minPosition.x, maxPosition.y, transform.position.z), new Vector3(maxPosition.x, maxPosition.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(maxPosition.x, maxPosition.y, transform.position.z), new Vector3(maxPosition.x, minPosition.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(maxPosition.x, minPosition.y, transform.position.z), new Vector3(minPosition.x, minPosition.y, transform.position.z));
+    }
 }
 
