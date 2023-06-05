400d3d7d9f187c1498415bcb3d8f686ee6684172
klocinho <lubiejedzeniexd@gmail.com>
2023-06-05 09:30:21 +0200

    Goofy Ahh muzyka i czyszczenie folderów

--- Assets/SKRYPTY/MENU/SettingsMenu.cs
+++ Assets/SKRYPTY/MENU/SettingsMenu.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer mainMixer;
+    public void SetVolume(float Volume)
+    {
+        mainMixer.SetFloat("volume", Volume);
+    }
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
---
+++ Assets/SKRYPTY/sledzenie.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sledzenie : MonoBehaviour
+{
+
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform postac;
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(postac.position.x, transform.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+
+}
+
